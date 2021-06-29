46bab4be7fa976c2f99b82409daf421238a9bcd4
Ascênio Sanderson <ascenio@hotmail.com>
2021-06-29 01:58:20 -0300

    fix: makes the tests test something

--- test/Validatorless_test.dart
+++ test/Validatorless_test.dart
@@ -3,7 +3,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:validatorless/validatorless.dart';
 
 void main() {
-  test('validate values', () {
-    Validatorless.required('');
+  group('required', () {
+    test("rejects when it's empty", () {
+      final expectedError = 'this field can not be empty';
+      final validator = Validatorless.required(expectedError);
+      final error = validator('');
+      expect(error, expectedError);
+    });
+
+    test("rejects when it's null", () {
+      final expectedError = 'this field can not be empty';
+      final validator = Validatorless.required(expectedError);
+      final error = validator(null);
+      expect(error, expectedError);
+    });
+
+    test("accepts when it's not empty", () {
+      final validator = Validatorless.required('this field can not be empty');
+      final error = validator('valid text');
+      expect(error, isNull);
+    });
   });
 }
